100ab04f0901b38614805f87599eb039416d19fa
Yannick Emminghaus <yannick@zintredge.de>
2023-02-15 16:04:04 +0100

    Commit better Version

--- collector.py
+++ collector.py
@@ -1,14 +1,16 @@
 """
+Check release version of Ubuntu.
 Run apt list and parse the output to get a list of installed packages including the version number.
 Create a list of the packages including the version number and the hostname of the machine.
 Connect to the MSSQL DB. Get the connection string from the a file called /etc/UpdTrack/db.pwd.
 If the table hostnames doesn't exist, create it.
 If the table packages doesn't exist, create it.
-Insert the hostname into the hostnames table.
-Insert the hostname, package name including version number and current date and time into the packages table.
+Insert the hostname into the hostnames table. Change the release version of Ubuntu for the hostname if it is different.
+Insert the release version of Ubuntu into the hostnames table.
+Insert the hostname, package name including version number and current date and time into the packages table. Mark the package as installed.
 If the hostname already exists in the hostnames table, don't insert it.
 If the combination of hostname and package name already exist in the packages table, don't insert it.
-If the hostname and package name including the package version don't exist in the list of packages, delete them from the packages table.
+If the hostname and package name including the package version don't exist in the list of packages, mark the package as uninstalled.
 Install this script as a service to run every 15 minutes.
 Run the service as root.
 """
@@ -28,6 +30,9 @@ now = datetime.datetime.now()
 
 # Get the current date and time in a format that MSSQL can use
 now = now.strftime("%Y-%m-%d %H:%M:%S")
+
+# Get the release version of Ubuntu
+release = subprocess.check_output(['lsb_release', '-r']).decode('utf-8').split()[1]
 
 # Get the output of the apt list command
 apt_list = subprocess.check_output(['apt', 'list', '--installed'])
@@ -65,7 +70,9 @@ cursor = conn.cursor()
 cursor.execute('''
     IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='hostnames' AND xtype='U')
     CREATE TABLE hostnames (
-        hostname VARCHAR(255) NOT NULL PRIMARY KEY
+        hostname VARCHAR(255) NOT NULL,
+        release VARCHAR(255) NOT NULL,
+        PRIMARY KEY (hostname)
     )
 ''')
 
@@ -76,6 +83,7 @@ cursor.execute('''
         hostname VARCHAR(255) NOT NULL,
         package VARCHAR(255) NOT NULL,
         date DATETIME NOT NULL,
+        installed BIT NOT NULL,
         PRIMARY KEY (hostname, package)
     )
 ''')
@@ -86,17 +94,23 @@ cursor.execute('''
     INSERT INTO hostnames (hostname) VALUES (?)
 ''', hostname, hostname)
 
-# Insert every package in the installed_packages list into the packages table linking it to the hostname
+# Insert the release version of Ubuntu into the hostnames table
+cursor.execute('''
+    UPDATE hostnames SET release = ? WHERE hostname = ?
+''', release, hostname)
+
+
+# Insert every package in the installed_packages list into the packages table linking it to the hostname marking it as installed
 for package in installed_packages:
     cursor.execute('''
         IF NOT EXISTS (SELECT * FROM packages WHERE hostname = ? AND package = ?)
-        INSERT INTO packages (hostname, package, date) VALUES (?, ?, ?)
-    ''', hostname, package, hostname, package, now)
+        INSERT INTO packages (hostname, package, date, installed) VALUES (?, ?, ?, ?)
+    ''', hostname, package, hostname, package, now, 1)
 
-#For every package in the packages table delete the entry if the package doesn't exist in the installed_packages list for the current machine
-for package in cursor.execute('SELECT package FROM packages WHERE hostname = ?', hostname):
-    if package not in installed_packages:
-        cursor.execute('DELETE FROM packages WHERE hostname = ? AND package = ?', hostname, package)
+# Check if there are any packages in the packages table that are not in the installed_packages list and mark them as uninstalled updating the date
+cursor.execute('''
+    UPDATE packages SET date = ?, installed = ? WHERE hostname = ? AND package NOT IN (SELECT * FROM ?)
+''', now, 0, hostname, installed_packages)
 
 # Commit the changes
 conn.commit()
